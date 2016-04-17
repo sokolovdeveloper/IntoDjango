1015681138a70adfe59d7a7eb0fc0acd682852be
grigory <sokolovdeveloper@gmail.com>
2016-04-18 00:29:57 +0300

    Merge branch 'master' of https://github.com/sokolovdeveloper/IntoDjango
    Conflicts:
    	polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
 
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, World! You're at the polls index.")
-=======
->>>>>>> edac36cd8203cf40b29d8dee6a7ef8a09d637321
