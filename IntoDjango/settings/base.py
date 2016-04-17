64e0002ec934bb0aad4427df7fa8447ed06011ff
grigory <sokolovdeveloper@gmail.com>
2016-04-18 00:46:06 +0300

    polls.apps

--- IntoDjango/settings/base.py
+++ IntoDjango/settings/base.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -114,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = None
 
 USE_I18N = True
 
